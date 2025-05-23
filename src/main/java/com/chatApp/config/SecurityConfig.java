7cb7435a6af35f0c73af69584292a1d8300f6444
Devarsh Doshi <devarsh.d2@ahduni.edu.in>
2025-05-23 23:51:49 +0530

    Google-Login

--- src/main/java/com/chatApp/config/SecurityConfig.java
+++ src/main/java/com/chatApp/config/SecurityConfig.java
@@ -4,20 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/test","/users/**").permitAll()
-                .anyRequest().authenticated()
-            )
-            .formLogin(form -> form.disable()) // disable login form
-            .httpBasic(httpBasic -> httpBasic.disable()); // also disable basic auth
+        http.csrf(csrf->csrf.disable())
+        .authorizeHttpRequests(authorizeRequests->
+        authorizeRequests.anyRequest().authenticated())
+        .oauth2Login(oauth2->oauth2.defaultSuccessUrl("/test",true));
 
         return http.build();
     }
